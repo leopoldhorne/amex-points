9133b944696452607975d115cc1390255cf64932
leopoldhorne <leopoldhorne1@gmail.com>
2025-11-02 00:11:08 -0400

    small bug

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -15,14 +15,14 @@ const Footer = () => {
     <footer className="bg-amber-50 py-5">
       <div className="mx-auto max-w-6xl flex justify-around max-sm:flex-col-reverse max-sm:items-center">
         <div className="flex flex-col max-w-[50%] px-2 max-sm:max-w-[80%] max-sm:justify-center w-full">
-            <h6 className="text-3xl max-sm:text-center max-sm:mt-5">About this project</h6>
-            <p className="max-sm:text-center mt-2 text-sm">I built this Amex Points tool because I was tired of doing the math every time I wanted to know what my points were worth. With so many transfer partners and conversion rates, it was hard to compare value at a glance. This project makes it easy to quickly see how far your points can go across different vendors without spreadsheets or extra effort.</p>
+            <h6 className="text-3xl max-sm:text-center max-sm:mt-5 text-black">About this project</h6>
+            <p className="max-sm:text-center mt-2 text-sm text-black">I built this Amex Points tool because I was tired of doing the math every time I wanted to know what my points were worth. With so many transfer partners and conversion rates, it was hard to compare value at a glance. This project makes it easy to quickly see how far your points can go across different vendors without spreadsheets or extra effort.</p>
         </div>
         <div className="flex flex-col items-center max-sm:max-w-[80%] w-full">
-            <h6 className="text-3xl mt-2">Disclaimer</h6>
+            <h6 className="text-3xl mt-2 text-black">Disclaimer</h6>
             <ul>
                 {
-                    disclaimer.map((elem, index) => <li key={index} className="flex gap-2 items-center max-w[80%] text-sm px-2 mt-1"><FontAwesomeIcon icon={faCircleCheck} className="h-4 w-4"/><span className="w-[85%]">{elem}</span></li> )
+                    disclaimer.map((elem, index) => <li key={index} className="flex gap-2 items-center max-w[80%] text-sm px-2 mt-1 text-black"><FontAwesomeIcon icon={faCircleCheck} className="h-4 w-4"/><span className="w-[85%]">{elem}</span></li> )
                 }
             </ul>
         </div>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,10 +5,10 @@ const Header = () => {
     <header className="bg-amber-50 w-screen h-screen max-sm:h-fit">
       <div className=" mx-auto max-w-6xl flex justify-center items-center h-full w-full max-sm:flex-col max-sm:max-w-[80%]">
         <div className="flex flex-col items-center gap-1 justify-center">
-          <h1 className="title text-5xl font-bold text-center max-sm:mt-10">
+          <h1 className="title text-5xl font-bold text-center max-sm:mt-10 text-black">
             AMEX Points Visualizer
           </h1>
-          <p className="text-xl text-center w-[80%] mt-5">
+          <p className="text-xl text-center w-[80%] mt-5 text-black">
            Turn confusion into clarity. Instantly see how much your Amex points are worth across top airlines, hotels, and redemption partners
           </p>
         </div>
